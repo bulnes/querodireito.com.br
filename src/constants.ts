e0a664b898a51128b2194d3299270f5846a7842e
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-12-06 01:19:10 -0300

    feat: improve seo

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const SITE_URL = 'https://querodireito-com-br.vercel.app';
+export const SITE_NAME = "Quero Direito";
+export const SITE_TITLE = "Quero Direito - Advogado Trabalhista Online";
+export const SITE_DESCRIPTION = "Precisa falar com um advogado online gratuitamente? Aqui na Quero Direito você fala com os melhores advogados trabalhista online. Ligue ou chame no Whatsapp";
+
+export const DEFAULT_PHONE = "1151232482";
+export const DEFAULT_WHATSAPP_PHONE = `55${DEFAULT_PHONE}`;
+export const DEFAULT_WHATSAPP_MESSAGE = "Olá, gostaria de falar com um advogado trabalhista online.";
