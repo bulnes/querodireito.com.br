40d5cf6436482a258f96c4560d733f9bb17ac57e
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-12-06 19:31:27 -0300

    feat: create fonts file

---
+++ src/fonts.ts
@@ -0,0 +1,11 @@
+import { Montserrat, Open_Sans } from "next/font/google";
+
+export const openSans = Open_Sans({
+  variable: "--font-open-sans",
+  subsets: ["latin"],
+});
+
+export const montserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
