1bc9b85c2836ab5cb218263297366f9740dd7196
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-12-06 19:41:49 -0300

    feat: install shadcnui

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,12 @@
-import { Logo } from "./logo";
+import { Logo } from "@/components/logo";
+import { Menu } from "@/components/menu";
 
 export function Header() {
   return (
     <header className="bg-qd-500 text-qd-100">
       <div className="centered-container h-16 flex flex-row items-center justify-between">
         <Logo />
-        <p>Menu</p>
+        <Menu />
       </div>
     </header>
   );
---
+++ src/components/menu.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Menu as MenuIcon } from "lucide-react";
+
+export function Menu() {
+  return (
+    <div>
+      <MenuIcon />
+    </div>
+  );
+}
