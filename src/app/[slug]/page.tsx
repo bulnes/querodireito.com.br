640015a43ed0a59999b0d6e9ab3225ef590797e4
Bruno Bulnes <bulnes.bruno@gmail.com>
2025-12-14 11:18:06 -0300

    fix: l2-l3 breadcrumb

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const { slug } = await params;
-  const pathname = `/${[...slug].join("/")}`;
+  const pathname = Array.isArray(slug) ? `/${slug.join("/")}` : `/${slug}`;
 
   return (
     <>
